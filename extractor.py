4880e9c35e5bb03576146ab84f3a03f086361c54
Damian Brito <brito314@gmail.com>
2021-09-06 14:32:01 -0400

    Need to trnsfer over functions to pcap_etl as part of Load class

--- extractor.py
+++ extractor.py
@@ -4,7 +4,7 @@ from glob import glob
 from concurrent.futures import ProcessPoolExecutor
 from subprocess import Popen
 import pandas as pd
-from pcap_cleaner import pCleaner, pStats
+from pcap_etl import pCleaner, pStats
 
 
 cleaner = pCleaner()
@@ -49,16 +49,16 @@ def cln_pkt_df_data(dataframe):
 # TODO: create channel df that contains rssi, noise, utilization
 
 
-def writeToDB(dataframe, parquet_store, fl, tp):
-    raw_data = dataframe.copy()
-    raw_data["floor"] = fl
-    raw_data["location"] = tp
-    ssids = stats.get_ssids(dataframe)
-    chanLst = pStats.get_chans(dataframe)
-    chanDict = dict(map(stats.get_ch_df, dataframe, chanLst))
-    beacons = stats.get_bcons(dataframe)
-    packet_types = ["mgmt", "data", "ctrl"]
-    pTypeDict = dict(map(stats.get_type_frames, dataframe, packet_types))
+# def writeToDB(dataframe, parquet_store, fl, tp):
+#     raw_data = dataframe.copy()
+#     raw_data["floor"] = fl
+#     raw_data["location"] = tp
+#     ssids = stats.get_ssids(dataframe)
+#     chanLst = pStats.get_chans(dataframe)
+#     chanDict = dict(map(stats.get_ch_df, dataframe, chanLst))
+#     beacons = stats.get_bcons(dataframe)
+#     packet_types = ["mgmt", "data", "ctrl"]
+#     pTypeDict = dict(map(stats.get_type_frames, dataframe, packet_types))
 
 
 def get_raw_dataframe_by_id(folder_path, level, tp):
@@ -84,12 +84,12 @@ def get_raw_dataframe_by_id(folder_path, level, tp):
     return dataframe
 
 
-def get_merged_test_point(csv_file):
-    level = csv_file.split('\\')[-1].split('_')[0]
-    testpoint = csv_file.split('\\')[-1].split('_')[1]
-    tp_df = get_raw_dataframe_by_id(csv_file, level, testpoint)
-    cleaned_tp = (level, testpoint)
-    return tp_df, cleaned_tp
+# def get_merged_test_point(csv_file):
+#     level = csv_file.split('\\')[-1].split('_')[0]
+#     testpoint = csv_file.split('\\')[-1].split('_')[1]
+#     tp_df = get_raw_dataframe_by_id(csv_file, level, testpoint)
+#     cleaned_tp = (level, testpoint)
+#     return tp_df, cleaned_tp
 
 
 def get_levels_and_test_points_csv(csv_dir):
@@ -97,19 +97,23 @@ def get_levels_and_test_points_csv(csv_dir):
     return Counter([f.split('\\')[-1].split('_')[0] for f in csv_files])
 
 
+# def csv_converter(csv_folder, par_dir):
+#     level_dict = get_levels_and_test_points_csv(csv_folder)
+#     for level in level_dict.keys():
+#         level_files = glob(os.path.join(csv_folder, f'{level}*'))
+#         for file in level_files:
+#             tp_dataframe, testpoint = get_merged_test_point(file)
+#             # write_raw_dataframe_to_parquet(tp_dataframe, par_dir, *testpoint)
+#             try:
+#                 pass
+#                 # write_raw_dataframe_to_parquet(tp_dataframe, par_dir, *testpoint)
+#             except ValueError:
+#                 continue
+#         print("")
+
+
 def csv_converter(csv_folder, par_dir):
-    level_dict = get_levels_and_test_points_csv(csv_folder)
-    for level in level_dict.keys():
-        level_files = glob(os.path.join(csv_folder, f'{level}*'))
-        for file in level_files:
-            tp_dataframe, testpoint = get_merged_test_point(file)
-            # write_raw_dataframe_to_parquet(tp_dataframe, par_dir, *testpoint)
-            try:
-                pass
-                # write_raw_dataframe_to_parquet(tp_dataframe, par_dir, *testpoint)
-            except ValueError:
-                continue
-        print("")
+
 
 
 def tshark_tool(pcap_dir, csv_dir):
@@ -125,78 +129,80 @@ def PCAP_converter_dist(pcap_dir, csv_folder):
         executor.map(tshark_tool, pcaps, csvs)
 
 
-def create_database(dbName):
-    db_filename = dbName
-    db = sqlite3.connect(db_filename)
-    cursor = db.cursor()
-    beaconsQuery = """
-	CREATE TABLE beacons (
-		id INTEGER PRIMARY KEY ASC,
-		floor_id TEXT,
-		test_point INT,
-		BSSID TEXT,
-		SSID TEXT,
-		channel INT,
-		band TEXT,
-		utilization REAL,
-		ap_name TEXT,
-		num_clients INT, 
-		rssi INT, 
-		fcs TEXT)"""
-    dataframesQuery = """
-	CREATE TABLE data_frames (
-		id INTEGER PRIMARY KEY ASC,
-		floor_id TEXT,
-		test_point TEXT,
-		channel INT,
-		band TEXT,
-		DS TEXT,
-		TA TEXT, 
-		RA TEXT,
-		data_rate INT, 
-		data_size INT, 
-		duration REAL,
-		retry TEXT, 
-		fcs TEXT)
-	"""
-    frameinfoQuery = """
-	CREATE TABLE frame_info (
-		id INTEGER PRIMARY KEY ASC,
-		floor_id TEXT,
-		test_point TEXT,
-		band TEXT,
-		source TEXT, 
-		dest TEXT, 
-		frameType TEXT, 
-		channel INT,
-		retry INT,
-		fcs TEXT)
-	"""
-
-    try:
-        for table in [beaconsQuery, dataframesQuery, frameinfoQuery]:
-            cursor.execute(table)
-    except sqlite3.OperationalError:
-        raise sqlite3.OperationalError
-    db.commit()
-    db.close()
+# def create_database(dbName):
+#     db_filename = dbName
+#     db = sqlite3.connect(db_filename)
+#     cursor = db.cursor()
+#     beaconsQuery = """
+# 	CREATE TABLE beacons (
+# 		id INTEGER PRIMARY KEY ASC,
+# 		floor_id TEXT,
+# 		test_point INT,
+# 		BSSID TEXT,
+# 		SSID TEXT,
+# 		channel INT,
+# 		band TEXT,
+# 		utilization REAL,
+# 		ap_name TEXT,
+# 		num_clients INT,
+# 		rssi INT,
+# 		fcs TEXT)"""
+#     dataframesQuery = """
+# 	CREATE TABLE data_frames (
+# 		id INTEGER PRIMARY KEY ASC,
+# 		floor_id TEXT,
+# 		test_point TEXT,
+# 		channel INT,
+# 		band TEXT,
+# 		DS TEXT,
+# 		TA TEXT,
+# 		RA TEXT,
+# 		data_rate INT,
+# 		data_size INT,
+# 		duration REAL,
+# 		retry TEXT,
+# 		fcs TEXT)
+# 	"""
+#     frameinfoQuery = """
+# 	CREATE TABLE frame_info (
+# 		id INTEGER PRIMARY KEY ASC,
+# 		floor_id TEXT,
+# 		test_point TEXT,
+# 		band TEXT,
+# 		source TEXT,
+# 		dest TEXT,
+# 		frameType TEXT,
+# 		channel INT,
+# 		retry INT,
+# 		fcs TEXT)
+# 	"""
+#
+#     try:
+#         for table in [beaconsQuery, dataframesQuery, frameinfoQuery]:
+#             cursor.execute(table)
+#     except sqlite3.OperationalError:
+#         raise sqlite3.OperationalError
+#     db.commit()
+#     db.close()
 
 
 
 if __name__ == '__main__':
-    output = sys.argv[2]
-    sourceDir = sys.argv[1]
+    # output = sys.argv[2]
+    output = r"C:\Users\domin\Desktop\Folders\pcap_project\Post_Processing\output"
+    # sourceDir = sys.argv[1]
+    sourceDir = r"C:\Users\domin\Desktop\Folders\pcap_project\Post_Processing\sample"
     csvDir = os.path.join(output, 'csv')
     db_path = os.path.join(output, 'pcap_db.db')
 
-    try:
-        os.mkdir(csvDir)
-    except FileExistsError:
-        pass
-    try:
-        create_database(db_path)
-    except sqlite3.OperationalError:
-        pass
+    # try:
+    #     os.mkdir(csvDir)
+    # except FileExistsError:
+    #     pass
+    # try:
+    #     create_database(db_path)
+    # except sqlite3.OperationalError:
+    #     pass
     PCAP_converter_dist(sourceDir, csvDir)
     csv_converter(csvDir, output)
     print("")
