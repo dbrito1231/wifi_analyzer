7d4df6fe9652548ec338a5592c2d17ed579c148c
Damian Brito <brito314@gmail.com>
2021-09-27 23:04:50 -0400

    Cleaned up script to show when it goes through stages

--- newMain.py
+++ newMain.py
@@ -9,12 +9,18 @@ import pandas as pd
 if __name__ == '__main__':
     pcap_dir = sys.argv[1]
     c_dir = sys.argv[2]
-    Extract().convert_pcap(pcap_dir,c_dir)
+    print(f"\n{pcap_dir}")
+    print(f"\n{c_dir}")
+    Extract().convert_pcap(pcap_dir, c_dir)
     loader = Load(c_dir)
     for file in glob.glob(fr"{c_dir}\*.csv"):
         print(file)
+        print("Munging data")
         df = Transform().mung(file)
         print(df)
+        print("saving file to csv")
         loader.save_file(df)
+    print("adding to database")
     loader.update_pg("pcap")
+    print("completed")
     print("")
