438cbadc0b10e461fade20d635b8680a777db5a8
Damian Brito <brito314@gmail.com>
2021-09-30 08:03:27 -0400

    Set in timer functions to measure class execution time

--- newMain.py
+++ newMain.py
@@ -1,26 +1,34 @@
 import glob
-
+import time
 from pcap_etl import Transform, Extract, Load
 import sys
-import os
-import pandas as pd
 
 
 if __name__ == '__main__':
     pcap_dir = sys.argv[1]
     c_dir = sys.argv[2]
     print(f"\n{pcap_dir}")
-    print(f"\n{c_dir}")
+    print(f"{c_dir}")
+    start_time = time.time()
     Extract().convert_pcap(pcap_dir, c_dir)
+    end_time = time.time()
+    print(f"\nExtractor start time: {start_time}")
+    print(f"Extractor end time: {end_time}")
+    print(f"duration: {end_time - start_time}")
     loader = Load(c_dir)
+    start_time = time.time()
     for file in glob.glob(fr"{c_dir}\*.csv"):
-        print(file)
-        print("Munging data")
         df = Transform().mung(file)
-        print(df)
-        print("saving file to csv")
         loader.save_file(df)
-    print("adding to database")
+    end_time = time.time()
+    print(f"\nTransform start time: {start_time}")
+    print(f"Transform end time: {end_time}")
+    print(f"duration: {end_time - start_time}")
+    start_time = time.time()
     loader.update_pg("pcap")
+    end_time = time.time()
+    print(f"\nLoad start time: {start_time}")
+    print(f"Load end time: {end_time}")
+    print(f"duration: {end_time - start_time}")
     print("completed")
     print("")
